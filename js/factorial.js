c29890709691bf943d08610b8a9dd5a975b900e1
robertuptc <robertuptc@gmail.com>
2024-05-31 12:32:35 -0400

    finished problem set

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,9 @@
-exports.factorial = function(num) {
+factorial = function(num) {
+    if (num == 0) {
+        return 1;
+    } else {
+        return num * factorial(num - 1)
+    }
 };
+
+module.exports = {factorial}
--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,6 @@
 def factorial(num):
 	# your code here
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
